cd5165113f1b440c93d12076e05e8c5e5cb62b0f
masahiro shimmori <shinmasa0412@yahoo.co.jp>
2019-07-19 16:19:45 +0900

    次へと前へのリンクが間違っていたので修正

--- index.php
+++ index.php
@@ -71,7 +71,7 @@ $to = ($offset + COMMENTS_PER_PAGE) < $total ? ($offset + COMMENTS_PER_PAGE) : $
           <?php endforeach; ?>
   </ul>
       <?php if ($page > 1) : ?>
-      <a href="?=page<?php echo $page-1;?>">前へ</a>
+      <a href="?page=<?php echo $page-1;?>">前へ</a>
       <?php endif ;?>
       <?php for($i = 1; $i <= $totalPages; $i++) :?>
         <?php if($page == $i) : ?>
@@ -81,7 +81,7 @@ $to = ($offset + COMMENTS_PER_PAGE) < $total ? ($offset + COMMENTS_PER_PAGE) : $
         <?php endif; ?>
       <?php endfor ;?>
       <?php if ($page < $totalPages) :?>
-      <a href="?=page<?php echo $page+1;?>">次へ</a>
+      <a href="?page=<?php echo $page+1;?>">次へ</a>
       <?php endif ;?>
 </body>
 </html>
